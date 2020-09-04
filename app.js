b7a131aca865ef0b5ca54ad2ef886fcbc8790162
Romulus Urakagi Tsai <urakagi@gmail.com>
2020-09-04 15:26:34 +0800

    Can run and get newman result now

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 const http = require('http');
+const newman = require('newman');
+const Collection = require('postman-collection').Collection;
 
-const options = {
-    host: process.env['api_host'],
-    port: process.env['api_port'],
+const host = process.env['api_host'];
+const port = process.env['api_port'];
+const headers = { Authorization: `Bearer ${process.env['jwt_token']}` }
+
+const optionsInit = {
+    host,
+    port,
     path: `/repositories/${process.env['repo_id']}/id`,
-    headers: {
-        Authorization: `Bearer ${process.env['jwt_token']}`
-    }
+    headers
 }
 
-const callback = function(response) {
+const callbackInit = function(response) {
     var resp = '';
-
     response.on('data', function (chunk) {
         resp += chunk;
     });
-
     response.on('end', function () {
-        console.log(resp);
-        const projectId = parseInt(JSON.parse(resp)['data'])
-        console.log(projectId);
+        const projectId = parseInt(JSON.parse(resp).data);
+        http.request(optionsForCollection(projectId), cbForCollection).end();
     });
 }
 
-http.request(options, callback).end();
+const cbForCollection = function(response) {
+    var resp = '';
+    response.on('data', function (chunk) {
+        resp += chunk;
+    });
+    response.on('end', function () {
+        const collection = JSON.parse(resp).data;
+        runNewman(collection);
+    });
+}
+
+function optionsForCollection(projectId) {
+    return {
+        host,
+        port,
+        path: `/export_to_postman/${projectId}`,
+        headers
+    }
+}
+
+function runNewman(collection) {
+    // console.log(collection);
+    newman.run({
+        collection: new Collection(collection)
+    }, (err, summary) => {
+        if (err) { throw err; }
+        const total = summary.run.stats.assertions.total;
+        const failed = summary.run.stats.assertions.failed;
+        console.log(`${failed}/${total} assertions failed.`);
+    });
+}
+
+http.request(optionsInit, callbackInit).end();
