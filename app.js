792e2d39f4f3bbee344477770098dcf88dcde028
Romulus Urakagi Tsai <urakagi@gmail.com>
2020-12-07 16:21:04 +0800

    Use repo symbolic link rather than download from gitlab Add commit_id field Remove git_token environment

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const fs = require('fs');
 
 const COLLECTION_FNAME = 'postman_collection.json';
 const ENVIRONMENT_FNAME = 'postman_environment.json'
-const STORAGE_PREFIX = 'iiidevops/postman/';
+const STORAGE_PREFIX = 'repo/iiidevops/postman/';
 
 const origin = process.env['api_origin'];
 const target = process.env['test_origin'];
 const git = {
   url: process.env['git_url'],
   pUrl: parse(process.env['git_url']),
-  token: process.env['git_token'],
-  branch: process.env['git_branch']
-} 
-const verbose = process.env['verbose'] == 'true';
+  branch: process.env['git_branch'],
+  commit_id: process.env['git_commit_id']
+}
+const verbose = process.env['verbose'] === 'true';
 const global = {
   jwtToken: null,
   repoId: -1,
@@ -26,86 +26,57 @@ const global = {
   report: {}
 }
 
+function apiGet(path, headers) {
+  return new Promise((resolve, reject) => {
+    if (!headers) {
+      headers = {};
+    }
+    headers['Authorization'] = `Bearer ${global.jwtToken}`;
+    const opts = {headers}
+    fetch(process.env['api_origin'] + path, opts)
+      .then(res => res.json())
+      .then(json => resolve(json))
+      .catch(err => console.error(err));
+  })
+}
+
+function apiPost(path, headers, body) {
+  return new Promise((resolve, reject) => {
+    if (!headers) {
+      headers = {};
+    }
+    headers['Authorization'] = `Bearer ${global.jwtToken}`;
+    const params = new URLSearchParams();
+    for (let key in body) {
+      params.append(key, body[key])
+    }
+    const opts = {method: 'POST', headers, body: params}
+    fetch(process.env['api_origin'] + path, opts)
+      .then(res => res.json())
+      .then(json => resolve(json))
+      .catch(err => console.error(err));
+  })
+}
+
 if (verbose) {
   console.log('Log into API server...');
 }
 
-const params = new URLSearchParams();
-params.append('username', process.env['username']);
-params.append('password', process.env['password']);
-fetch(`${origin}/user/login`, {
-  method: 'POST',
-  body: params
-})
-.then(res => res.json())
-.then(json => {
+apiPost('/user/login', null, {
+  username: process.env['username'],
+  password: process.env['password']
+}).then(json => {
   global.jwtToken = json.data.token;
-  if (verbose) {
-    console.log('Retrieving repo_id...');
-  }
-  getGitlabProjectId(1)
-    .then(id => {
-      if (verbose) console.log('repo_id is ' + id);
-      global.repoId = id;
-      getProjectId(id);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
-
-function getGitlabProjectId(page) {
-  return new Promise((resolve, reject) => {
-    let ret = -1;
-    fetch(git.pUrl.origin +
-      '/api/v4/projects?simple=true&per_page=50&page=' + page,
-      {
-        headers: {'PRIVATE-TOKEN': git.token}
-      })
-      .then(res => res.json())
-      .then(json => {
-        for (p in json) {
-          const proj = json[p];
-          if (proj.http_url_to_repo == git.url) {
-            ret = proj.id;
-            break;
-          }
-        }
-        if (ret < 0) {
-          if (json.length < 20) {
-            reject(`Git URL ${git.url} not found!\nUse URL with suffix .git`);
-          } else {
-            getGitlabProjectId(page + 1)
-              .then(json => resolve(json));
-          }
-        } else {
-          resolve(ret);
-        }
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-}
-
-function getProjectId(repoId) {
-  fetch(`${origin}/repositories/${repoId}/id`, {
-    method: 'GET',
-    headers: {Authorization: `Bearer ${global.jwtToken}`}
-  })
-    .then(res => res.json())
+  if (verbose) console.log('Retrieving repo_id...');
+  apiGet(`/repositories/id?repository_url=${git.url}`)
     .then(json => {
-      global.projectId = json.data;
-      if (verbose) console.log('project id is ' + global.projectId);
-      fetch(`${origin}/export_to_postman/${global.projectId}?target=${
-          encodeURIComponent(target)}`,
-        {
-          method: 'GET',
-          headers: {Authorization: `Bearer ${global.jwtToken}`}
-        })
-        .then(res => res.json())
+      global.projectId = json.data.project_id;
+      global.repoId = json.data.repository_id;
+      if (verbose) console.log('repo_id is ' + global.repoId);
+      apiGet(`/export_to_postman/${global.projectId}?target=${
+        encodeURIComponent(target)}`)
         .then(json => {
-          if (json.message != 'success') {
+          if (json.message !== 'success') {
             throw Error(JSON.stringify(json));
           }
           const data = json.data;
@@ -120,8 +91,8 @@ function getProjectId(repoId) {
         .catch(err => {
           console.error(err);
         });
-    });
-}
+    })
+});
 
 function runNewmanInAPIDB(filename) {
   const options = {collection: require('./' + filename)}
@@ -164,57 +135,30 @@ function reduceSummary(s) {
   return ret;
 }
 
-function gitFileAPIUrl(path) {
-  return `${git.pUrl.origin}/api/v4/projects/${global.repoId}`
-    + `/repository/files/${path}/raw?ref=${git.branch}`
-}
-
 function checkCollectionFile() {
-  const collectionPath = encodeURIComponent(STORAGE_PREFIX + COLLECTION_FNAME);
-  fetch(gitFileAPIUrl(collectionPath),
-    {headers: {'PRIVATE-TOKEN': git.token}})
-    .then(res => {
-      if (res.status === 404) {
-        if (verbose) console.log('No collection file found.');
-        uploadResult(global.projectId, global.total, global.failed)
-        return;
-      }
-      if (verbose) console.log("Collection file found.");
-      const dest = fs.createWriteStream('./' + COLLECTION_FNAME);
-      res.body.pipe(dest);
-      downloadEnvironment();
-    })
-    .catch(err => {
-      console.error(err);
-    });
-}
-
-function downloadEnvironment() {
-  const environmentPath = encodeURIComponent(STORAGE_PREFIX + ENVIRONMENT_FNAME);
-  fetch(gitFileAPIUrl(environmentPath),
-    {headers: {'PRIVATE-TOKEN': git.token}})
-    .then(res => {
-      if (res.status === 404) {
-        if (verbose) console.log('No environment file found.');
-        uploadResult(global.projectId, global.total, global.failed)
-        return;
-      }
-      if (verbose) console.log("Environment file found.");
-      const dest = fs.createWriteStream('./' + ENVIRONMENT_FNAME);
-      res.body.pipe(dest);
-      runNewmanByJSON();
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  const collectionPath = STORAGE_PREFIX + COLLECTION_FNAME;
+  if (!fs.existsSync(collectionPath)) {
+    if (verbose) console.log('No collection file found.');
+    uploadResult(global.projectId, global.total, global.failed)
+    return;
+  }
+  if (verbose) console.log("Collection file found.");
+  const environmentPath = STORAGE_PREFIX + ENVIRONMENT_FNAME;
+  if (!fs.existsSync(environmentPath)) {
+    if (verbose) console.log('No environment file found.');
+    uploadResult(global.projectId, global.total, global.failed)
+    return;
+  }
+  if (verbose) console.log("Environment file found.");
+  runNewmanByJSON();
 }
 
 function runNewmanByJSON() {
   if (verbose) console.log("Running newman for json files...");
   const options = {
-    collection: './' + COLLECTION_FNAME,
-    environment: './' + ENVIRONMENT_FNAME,
-    envVar: [{ key: 'test_origin', value: target }]
+    collection: STORAGE_PREFIX + COLLECTION_FNAME,
+    environment: STORAGE_PREFIX + ENVIRONMENT_FNAME,
+    envVar: [{key: 'test_origin', value: target}]
   }
   if (verbose) options['reporters'] = 'cli';
   newman.run(options, (err, summary) => {
@@ -228,10 +172,12 @@ function runNewmanByJSON() {
     }
     global.total += summary.run.stats.assertions.total;
     global.failed += summary.run.stats.assertions.failed;
-    fs.unlink('./' + COLLECTION_FNAME, () => {});
-    fs.unlink('./' + ENVIRONMENT_FNAME, () => {});
-    uploadResult(global.projectId, global.total, 
-        global.failed, JSON.stringify(global.report));
+    fs.unlink('./' + COLLECTION_FNAME, () => {
+    });
+    fs.unlink('./' + ENVIRONMENT_FNAME, () => {
+    });
+    uploadResult(global.projectId, global.total,
+      global.failed, JSON.stringify(global.report));
   });
 }
 
@@ -241,6 +187,7 @@ function uploadResult(projectId, total, failed, report) {
   params.append('total', total);
   params.append('fail', failed);
   params.append('branch', git.branch);
+  params.append('commit_id', git.commit_id);
   params.append('report', report);
   fetch(
     `${origin}/testResults`,
